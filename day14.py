4b9e27634d576e37e46b1287c927c4501dcb7d54
Katherine Hagerty <katherine.hagerty@boeing.com>
2023-12-14 16:45:00 -0600

    Solved Day 14 part 1

--- day14.py
+++ day14.py
@@ -11,22 +11,23 @@ with open('day14_input.txt', 'r') as f:
     f.close()
     
 def part1():
+    # Transpose input data
+    # Working with data will be easier in rows
     d = [list(i) for i in data]
     transposed = [list(i) for i in zip(*d)]
-    transposed = [''.join(i) for i in transposed]
-    transposed.reverse()
     
+    # Reverse each line of data
+    # Now the index in string cooresponds to distance from south edge
+    transposed = [''.join(reversed(i)) for i in transposed]
+    
+    # Tilt the rocks
     splits = [list(i.split('#')) for i in transposed]
-    tilted_positions = [sorted(i) for i in splits]
+    tilted_positions = [[''.join(sorted(list(j))) for j in i] for i in splits]
     tilted_string = ['#'.join(i) for i in tilted_positions]
-    ind_values = [j.span()[1] + 1 for i in tilted_string for j in re.finditer('O',i)]
+    
+    # Add all the indices of the rocks
+    ind_values = [j.span()[1] for i in tilted_string for j in re.finditer('O',i)]
     
     return sum(ind_values)
-    # for i in tilted_string:
-    #     for j in re.findall('O', i):
-    #         j.span()[1]
-                     
-    # for sent in text:
-    #     for word in sent:
-    #         word
+
 answer1 = part1()
