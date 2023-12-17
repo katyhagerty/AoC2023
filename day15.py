d1f39bcd4a31ad5505dd22f5d2fde551551e093c
katyhagerty <katyhagerty93@gmail.com>
2023-12-17 14:52:42 -0600

    Solved day 15 part 2

--- day15.py
+++ day15.py
@@ -22,3 +22,51 @@ def part1():
     return answer
     
 answer1 = part1()
+
+def part2():
+    answer = 0
+    d= {}
+    for step in data.split(','):
+        box = 0
+        
+        if '-' in step:
+            label, length = step.split('-')
+            
+            for letter in label:
+                box += ord(letter)
+                box *= 17
+                box = box % 256
+            
+            if box in d.keys():
+                labels = [i for i in d[box] if label in i]
+                if len(labels) > 0:
+                    d[box].remove(*labels)
+            
+        else:
+            label, length = step.split('=')
+            
+            for letter in label:
+                box += ord(letter)
+                box *= 17
+                box = box % 256
+            
+            if box in d.keys():
+                labels = [i for i in d[box] if label in i]
+                if len(labels) > 0:
+                    old = labels[0]
+                    new = f'{label} {length}'
+                    ind = d[box].index(old)
+                    d[box].pop(ind)
+                    d[box].insert(ind,new)
+                else:
+                    d[box].append(f'{label} {length}')
+            else:
+                d[box] = [f'{label} {length}']
+    
+    for box in d.keys():
+        for i,lens in enumerate(d[box]):
+            label,length = lens.split()
+            answer += (box + 1) * (i + 1) * int(length)
+    
+    return answer
+answer2 = part2()
